ca260ffce21f1fb8b2477560848d3dc610e7f865
Joseph Ononaye <josephononaye@gmail.com>
2023-09-29 17:56:07 +0100

    Wrote code save user input and indicate with different colours each time block , whether past, present or future, in relation to present hour

--- script.js
+++ script.js
@@ -19,10 +19,30 @@ var currentHour = dayjs().format('H');
       // Funtion to collect and save user input
     function textEntry() {
       $('.saveBtn').on('click', function() {
-        const key = $(this).parent().attr('id');
-        const value = $(this).siblings('.description').val();
-        localStorage.setItem(key, value);
-      });
-    }})
+        var key = $(this).parent().attr('id');
+  var value = $(this).siblings('.description').val();
+    localStorage.setItem(key, value);
+              });
+    }
 
+     // Function to indicate the color of each time block based on past, present or future. 
+     function refreshColor() {
+        $('.time-block').each(function() {
+                  var blockHour = parseInt(this.id);
+            if (blockHour == currentHour) {
+            $(this).removeClass('past future').addClass('present');
+          } else if (blockHour < currentHour) {
+            $(this).removeClass('future present').addClass('past');
+          } else {
+            $(this).removeClass('past present').addClass('future');
+          }
+        });
+      }
+      // Collect user input to show the values for each time block.
+      $('.time-block').each(function() {
+        var key = $(this).attr('id');
+        var value = localStorage.getItem(key);
+        $(this).children('.description').val(value);
+      });
     
+    })
