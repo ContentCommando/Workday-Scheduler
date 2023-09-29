2d79f5e60a4c800aa2fc680fa8cf300014b76ff1
Joseph Ononaye <josephononaye@gmail.com>
2023-09-29 14:40:45 +0100

    Improved code to allow application collect and save user inputs

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-  // Put code in container using{}
-  var localeSettings = {};
-  dayjs.locale(localeSettings);
-  // Function to run when DOM is loaded.
-  $(function () {
-        // Using the dayjs library to get current time
-    var currentHour = dayjs().format('H');
-  // Function to change background color of each time block based on current hour.
+// Put code in container using{}
+var localeSettings = {};
+    dayjs.locale(localeSettings);
+
+// Function to run when DOM is loaded.
+    $(function () {
+// Using the dayjs library to get current time
+var currentHour = dayjs().format('H');
+// Function to change background color of each time block based on current hour.
     function hourlyColor() {
       $('.time-block').each(function() {
         var blockHour = parseInt(this.id);
@@ -13,4 +14,15 @@
         $(this).toggleClass('present', blockHour === currentHour);
         $(this).toggleClass('future', blockHour > currentHour);
       });
+    }
+
+      // Funtion to collect and save user input
+    function textEntry() {
+      $('.saveBtn').on('click', function() {
+        const key = $(this).parent().attr('id');
+        const value = $(this).siblings('.description').val();
+        localStorage.setItem(key, value);
+      });
     }})
+
+    
