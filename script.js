6b8501e2381f79ff2df7c3b3e79cae05ac1ef687
Joseph Ononaye <josephononaye@gmail.com>
2023-09-29 20:20:54 +0100

    Wrote code to display current time of day and called all stated functions within the js code base. Also restyled with CSS for better aesthetics.

--- script.js
+++ script.js
@@ -7,17 +7,18 @@ var localeSettings = {};
 // Using the dayjs library to get current time
 var currentHour = dayjs().format('H');
 // Function to change background color of each time block based on current hour.
-    function hourlyColor() {
+    function blockColour() {
       $('.time-block').each(function() {
         var blockHour = parseInt(this.id);
+        console.log(this.id);
         $(this).toggleClass('past', blockHour < currentHour);
         $(this).toggleClass('present', blockHour === currentHour);
         $(this).toggleClass('future', blockHour > currentHour);
       });
     }
 
-      // Funtion to collect and save user input
-    function textEntry() {
+// Funtion to collect and save user input
+    function userInput() {
       $('.saveBtn').on('click', function() {
         var key = $(this).parent().attr('id');
   var value = $(this).siblings('.description').val();
@@ -25,8 +26,8 @@ var currentHour = dayjs().format('H');
               });
     }
 
-     // Function to indicate the color of each time block based on past, present or future. 
-     function refreshColor() {
+// Function to indicate the color of each time block based on past, present or future. 
+     function colourUpdate() {
         $('.time-block').each(function() {
                   var blockHour = parseInt(this.id);
             if (blockHour == currentHour) {
@@ -38,11 +39,27 @@ var currentHour = dayjs().format('H');
           }
         });
       }
-      // Collect user input to show the values for each time block.
+// Collect user input to show the values for each time block.
       $('.time-block').each(function() {
         var key = $(this).attr('id');
         var value = localStorage.getItem(key);
         $(this).children('.description').val(value);
       });
+
+// Function to display current time of the day.
+    function updateTime() {
+        var dateElement = $('#date');
+        var timeElement = $('#time');
+        var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+        var currentTime = dayjs().format('hh:mm:ss A');
+        dateElement.text(currentDate);
+        timeElement.text(currentTime);
+      }
+
+// Call the functions
+      blockColour(); userInput(); colourUpdate();
+
+// Set clock go go live 
     
-    })
+      setInterval(updateTime, 1000);
+    });
